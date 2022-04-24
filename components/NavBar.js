8c1c6da925f34dd7b0bed314c271c393d9d4e1a0
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-04-24 15:09:49 +0200

    add navbar

---
+++ components/NavBar.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+const NavBar = () => (
+  <nav className="sticky mb-10 top-0 w-full flex justify-center items-center after:block after:absolute after:-bottom-[2px] after:w-[70px] after:h-[5px] after:bg-[#c9c9c9]">
+    <div className="w-2/3 h-32 flex justify-center items-center border-b border-[#c9c9c9]">
+      <Image
+        src="/vecna-logo-solo-100.png"
+        alt="Vecnost logo"
+        width={90}
+        height={100}
+        objectFit={"contain"}
+      />
+      <div className="font-serif text-3xl sm:text-5xl">Baza Podataka</div>
+    </div>
+  </nav>
+);
+
+export default NavBar;
