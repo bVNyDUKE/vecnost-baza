7b6512f6f0b87430580687e7fc408836f53be965
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-04-25 22:52:40 +0200

    md screen section content align

--- components/Section.js
+++ components/Section.js
@@ -23,22 +23,24 @@ const Section = ({
   };
 
   return (
-    <section className={`${styles[color].text}`}>
-      <div className={`py-5 text-center ${styles[color].header}`}>
-        <div className="md:top-32 md:origin-center md:-rotate-90 md:transform md:font-bold">
+    <section className={`md:flex md:h-[33vh] ${styles[color].text}`}>
+      <div className={`py-5 text-center md:w-1/5 ${styles[color].header}`}>
+        <div className="md:origin-bottom md:-rotate-90 md:transform md:font-bold">
           <h2 className="font-serif text-3xl font-[400]">{title}</h2>
-        </div>
-        <div className="text-sm font-thin uppercase tracking-wide">
-          {subtitle}
+          <div className="text-sm font-thin uppercase tracking-wide">
+            {subtitle}
+          </div>
         </div>
       </div>
       <div
-        className={`flex min-h-[175px] flex-col items-center space-y-10 p-5 ${styles[color].content}`}
+        className={`md:a flex min-h-[175px] flex-col items-center space-y-10 p-5 md:grow md:flex-row md:justify-center md:space-y-0  ${styles[color].content}`}
       >
-        <div className="text-center text-lg">{content}</div>
-        <Button>
-          <Link href={href}>{linkLabel}</Link>
-        </Button>
+        <div className="w-1/2 max-w-md text-center text-lg">{content}</div>
+        <div className="flex w-1/2 max-w-md justify-center text-sm">
+          <Button>
+            <Link href={href}>{linkLabel}</Link>
+          </Button>
+        </div>
       </div>
     </section>
   );
