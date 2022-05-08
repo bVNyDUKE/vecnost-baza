d1f2b0f59a9ebe934fcb1818f881ccd03b916777
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-05-08 22:24:00 +0200

    wip on data clean

---
+++ spreadsheets/cleanData.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+'''
+TODO Clean up 1300 kaplara (van groblja)
+'''
+  
+def idFromName(row, df):
+  return df[df['name'] == row].index.values[0]
+
+def idFromId(row, column, val):
+  return  persons[persons[column] == row.name][val].iat[0]
+
+org = pd.read_excel('spreadsheets/organizacija.xlsx', sheet_name='Opštine u Srbiji', names=['region', 'okrug', 'opstina'], usecols=[0, 1, 2])
+
+persons = pd.read_excel('spreadsheets/spisak.xlsx', sheet_name='Spisak').drop(['Spojnica', 'Godina', 'Mesto'], axis=1)
+persons.columns = 'pol,srpsko_prezime,srpsko_ime,srpski_nadimak,prezime,ime,srednje_slovo,nadimak,rodjenje,smrt,groblje,opstina,okrug,region'.split(',')
+persons = persons.replace('x', None)
+
+groblja = pd.DataFrame(persons['groblje'].unique(), columns=['name'] )
+opstine = pd.DataFrame(org['opstina'].unique(), columns=['name'] )
+okruzi = pd.DataFrame(org['okrug'].unique(), columns=['name'] )
+regioni = pd.DataFrame(org['region'].unique(), columns=['name'])
+
+persons['groblje'] = persons['groblje'].apply(idFromName, args=(groblja,))
+persons['opstina'] = persons['opstina'].apply(idFromName, args=(opstine,))
+persons['okrug'] = persons['okrug'].apply(idFromName, args=(okruzi,))
+persons['region'] = persons['region'].apply(idFromName, args=(regioni,))
+
+org['region_id'] = org['region'].apply(idFromName, args=(regioni, ))
+org['okrug_id'] = org['okrug'].apply(idFromName, args=(okruzi, ))
+org['opstina_id'] = org['opstina'].apply(idFromName, args=(opstine, ))
+
+
+groblja['opstina_id'] = groblja.apply(idFromId, args=('groblje', 'opstina',), axis=1)
+opstine['okrug_id'] = groblja.apply(idFromId, args=('opstina', 'okrug',), axis=1)
+okruzi['region_id'] = groblja.apply(idFromId, args=('okrug', 'region',), axis=1)
+print(groblja.head())
+print(opstine.head())
+print(okruzi.head())
+
+# persons.to_csv('spreadsheets/persons.csv', index_label='id')
+# groblja.to_csv('spreadsheets/groblja.csv', index_label='id')
+# opstine.to_csv('spreadsheets/opstine.csv', index_label='id')
+# regioni.to_csv('spreadsheets/regioni.csv', index_label='id')
+# okruzi.to_csv('spreadsheets/okruzi.csv', index_label='id')
