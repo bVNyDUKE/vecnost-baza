a39b5c17f35eb08f7821b8ec0974e32b8271839c
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-05-15 16:49:08 +0200

    fix issue with relationship loading

--- pages/indexMesta.js
+++ pages/indexMesta.js
@@ -6,7 +6,7 @@ export async function getServerSideProps() {
   const { data } = await supabase
     .from("region")
     .select(
-      "id, name, okrug ( id, name, opstina (id, name, groblje (id, name) ) ) "
+      "id, name, okrug!okrug_region_id_fkey ( id, name, opstina!opstina_okrug_id_fkey (id, name, groblje!groblje_opstina_id_fkey (id, name) ) )"
     );
   return { props: { data } };
 }
@@ -18,9 +18,8 @@ const IndexList = ({ data, title }) => {
     <div className="mt-5 mb-5 font-serif">
       <h2 className="ml-5 text-3xl">{title}</h2>
       <ul className="mt-2 ml-7 list-inside">
-        {data.map((entry) => (
-          <IndexEntry key={entry.id} entry={entry} />
-        ))}
+        {data &&
+          data.map((entry) => <IndexEntry key={entry.id} entry={entry} />)}
       </ul>
     </div>
   );
@@ -59,8 +58,7 @@ const IndexEntry = ({ entry }) => {
   return <li>{entry.name}</li>;
 };
 
-export default function Entry({ data }) {
-  console.log(data);
+export default function Entry({ data = [] }) {
   return (
     <div className="container mx-auto max-w-lg">
       <h1 className="text-center font-serif text-4xl">Index</h1>
