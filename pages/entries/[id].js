98b403fe6af3843a5df459f55692b67aa7db90ed
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-05-15 16:49:15 +0200

    add entry css

--- pages/entries/[id].js
+++ pages/entries/[id].js
@@ -2,15 +2,30 @@ import { supabase } from "../../utils/supabaseClient";
 
 export async function getServerSideProps(context) {
   const { id } = context.params;
-  const { data } = await supabase.from("persons").select().eq("id", id);
+  const { data } = await supabase
+    .from("persons")
+    .select(
+      "ime, prezime, nadimak, pol, rodjenje, smrt, groblje (name), opstina (name), okrug (name), region (name)"
+    )
+    .eq("id", id);
   return { props: { data: data[0] } };
 }
 
 export default function Entry({ data }) {
   return (
-    <div>
-      <h1>{data.ime}</h1>
-      <p>{data.prezime}</p>
+    <div className="container mx-auto max-w-lg space-y-2 p-2 font-serif">
+      <h1 className="text-2xl">
+        {data.ime} {data.prezime}
+      </h1>
+      <div className="text-2xl">
+        {data.rodjenje} - {data.smrt}
+      </div>
+      {data.nadimak && <div>{data.nadimak}</div>}
+      <div>Pol: {data.pol}</div>
+      <div>Groblje: {data.groblje.name}</div>
+      <div>Opština: {data.opstina.name}</div>
+      <div>Okrug: {data.okrug.name}</div>
+      <div>Region: {data.region.name}</div>
     </div>
   );
 }
