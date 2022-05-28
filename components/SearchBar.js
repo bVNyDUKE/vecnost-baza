935772fbc7e6ff8ba3059d03c49fa42e6cfb2114
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-05-28 15:38:11 +0200

    usecallback on search

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import Icon from "./Icons";
 import { supabase } from "../utils/supabaseClient";
 
@@ -6,22 +6,25 @@ var timeout;
 export default function SearchBar({ setResults }) {
   const [searching, setSearching] = useState(false);
 
-  function searchChange(e) {
-    clearTimeout(timeout);
-    setSearching(true);
-    timeout = setTimeout(async () => {
-      setSearching(false);
-      const cleanedInput = e.target.value
-        .replace(/dj/g, "đ")
-        .replace(/Dj/g, "Đ");
-      const { data } = await supabase
-        .from("persons")
-        .select("id, ime, prezime, rodjenje, smrt, groblje (name)")
-        .textSearch("fts", cleanedInput, { config: "sr", type: "websearch" });
-      setResults(data);
-      console.log(data);
-    }, 1000);
-  }
+  const onSearch = useCallback(
+    (e) => {
+      clearTimeout(timeout);
+      setSearching(true);
+      timeout = setTimeout(async () => {
+        setSearching(false);
+        const cleanedInput = e.target.value
+          .replace(/dj/g, "đ")
+          .replace(/Dj/g, "Đ");
+        const { data } = await supabase
+          .from("persons")
+          .select("id, ime, prezime, rodjenje, smrt, groblje (name)")
+          .textSearch("fts", cleanedInput, { config: "sr", type: "websearch" });
+        setResults(data);
+        console.log(data);
+      }, 1000);
+    },
+    [setResults]
+  );
 
   return (
     <div className="m-auto flex h-16 w-full max-w-3xl items-center space-x-8 rounded-md border border-gray-300 bg-white shadow-md">
@@ -32,8 +35,8 @@ export default function SearchBar({ setResults }) {
         type="text"
         name="search"
         id="search"
-        onChange={searchChange}
-        onKeyUp={(e) => (e.code = "Enter" && searchChange(e))}
+        onChange={onSearch}
+        onKeyUp={(e) => (e.code = "Enter" && onSearch(e))}
         placeholder="Pretraga..."
         className="flex-grow focus:outline-none"
       />
