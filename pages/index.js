01128955ecf1b110b17e9d94590e9b7d7c3142b9
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-05-29 16:43:24 +0200

    home page data inside componetn

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,30 @@
 import Section from "../components/Section";
 
-const data = [
-  {
-    title: "Pretraga",
-    subtitle: "U izradi",
-    content: "Pretražite sahranjena lica po imenu",
-    href: "/search",
-    linkLabel: "Idi na pretragu",
-  },
-  {
-    title: "Mapa",
-    subtitle: "U izradi",
-    content: "Pronađite arhivirane lokacija na mapi",
-    href: "",
-    linkLabel: "U izradi",
-  },
-  {
-    title: "Indeks",
-    subtitle: "U izradi",
-    content: "Indeks svih arhiviranih lokacija",
-    href: "/indexMesta",
-    linkLabel: "Idi na indeks",
-  },
-];
-
 export default function Home() {
+  const data = [
+    {
+      title: "Pretraga",
+      subtitle: "U izradi",
+      content: "Pretražite sahranjena lica po imenu",
+      href: "/search",
+      linkLabel: "Idi na pretragu",
+    },
+    {
+      title: "Mapa",
+      subtitle: "U izradi",
+      content: "Pronađite arhivirane lokacija na mapi",
+      href: "",
+      linkLabel: "U izradi",
+    },
+    {
+      title: "Indeks",
+      subtitle: "U izradi",
+      content: "Indeks svih arhiviranih lokacija",
+      href: "/indexMesta",
+      linkLabel: "Idi na indeks",
+    },
+  ];
+
   return (
     <>
       {data.map((section, index) => (
