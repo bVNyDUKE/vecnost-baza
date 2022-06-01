4e3adacdbc26d95f7055ab9ec39b743908dceacb
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-06-01 22:44:48 +0200

    update button links

--- components/Button.js
+++ components/Button.js
@@ -1,11 +1,24 @@
 import Link from "next/link";
 
-const Button = ({ href, label }) => (
-  <Link href={href}>
-    <a className="hover:pointer rounded-3xl bg-black px-7 py-2 font-bold uppercase text-white shadow-md hover:shadow-lg active:bg-[#404040]">
+const Button = ({ href, label, onClick = null }) => {
+  if (href) {
+    return (
+      <Link href={href}>
+        <a className="hover:pointer rounded-3xl bg-black px-7 py-2 font-bold uppercase text-white shadow-md hover:shadow-lg active:bg-[#404040]">
+          {label}
+        </a>
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      className="hover:pointer rounded-3xl bg-black px-7 py-2 font-bold uppercase text-white shadow-md hover:shadow-lg active:bg-[#404040]"
+    >
       {label}
-    </a>
-  </Link>
-);
+    </button>
+  );
+};
 
 export default Button;
