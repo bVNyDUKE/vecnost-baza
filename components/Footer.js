a373c469069b94d12b139ff9df395c18ca12d575
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-06-04 12:27:45 +0200

    add footer

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-primary-dark py-5 text-center text-white">
+      <div className="container mx-auto">
+        <div className="flex justify-center">
+          <div className="flex-1">&copy; 2020 Vecnost</div>
+        </div>
+      </div>
+    </footer>
+  );
+}
