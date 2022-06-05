7f464b9872028978163dc3f957271988482a9a76
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-06-05 16:12:33 +0200

    dynamic footer date

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,9 @@ export default function Footer() {
     <footer className="bg-primary-dark py-5 text-center text-white">
       <div className="container mx-auto">
         <div className="flex justify-center">
-          <div className="flex-1">&copy; 2022 Vecnost</div>
+          <div className="flex-1">
+            &copy; {new Date().getFullYear()} Vecnost
+          </div>
         </div>
       </div>
     </footer>
