8df5a428b76c0f0fc51b27d3640e30c36c11c9c9
Darko Ivkov <bVNyDUKE@protonmail.com>
2023-05-27 18:20:20 +0200

    add revalidation api

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
+    return res.status(403).send("Forbidden");
+  }
+
+  try {
+    const revalidateUrls = ["/statistika", "/pretraga", "/mapa", "/indeks"].map(
+      (url) => res.revalidate(url)
+    );
+    await Promise.all(revalidateUrls);
+    return res.json({ revalidated: true });
+  } catch (e) {
+    return res.status(500).send("Error in revalidation");
+  }
+}
