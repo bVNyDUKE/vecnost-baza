51a6f1120fd9c9c0ff1615887ef246cab1ca8ca4
Darko Ivkov <bVNyDUKE@protonmail.com>
2024-02-15 23:18:55 +0100

    cleaner footer

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,38 +1,32 @@
 import { Facebook, Instagram, Linkedin } from "../Icons";
 
+const socials = [
+  {
+    id: "Facebook",
+    href: "https://www.facebook.com/vecnostcom",
+    icon: <Facebook />,
+  },
+  {
+    id: "LinkedIn",
+    href: "https://www.linkedin.com/company/vecnostcom/",
+    icon: <Linkedin />,
+  },
+  {
+    id: "Instagram",
+    href: "https://www.instagram.com/vecnostcom/",
+    icon: <Instagram />,
+  },
+];
+
 export default function Footer() {
   return (
     <footer className="relative mt-16 bg-gradient-to-r from-[#000] to-[#3d3d3d] py-5 text-center text-white">
       <div className="absolute top-[-3rem] left-1/2 flex -translate-x-1/2 transform gap-4">
-        <a
-          target="_blank"
-          aria-label="Facebook"
-          href="https://www.facebook.com/vecnostcom"
-        >
-          <div>
-            <Facebook />
-          </div>
-        </a>
-
-        <a
-          target="_blank"
-          aria-label="LinkedIn"
-          href="https://www.linkedin.com/company/vecnostcom/"
-        >
-          <div>
-            <Linkedin />
-          </div>
-        </a>
-
-        <a
-          target="_blank"
-          aria-label="Instagram"
-          href="https://www.instagram.com/vecnostcom/"
-        >
-          <div>
-            <Instagram />
-          </div>
-        </a>
+        {socials.map((s) => (
+          <a target="_blank" aria-label={s.id} key={s.id} href={s.href}>
+            {s.icon}
+          </a>
+        ))}
       </div>
 
       <div className="container mx-auto">
